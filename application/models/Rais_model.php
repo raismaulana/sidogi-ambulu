9fd406379b97bc35e47f2766f1424824460ad4b0
Lufri Rais Maulana <lufriraismaulana@gmail.com>
2018-11-18 10:22:37 +0700

    data master v0
    crud

--- application/models/Rais_model.php
+++ application/models/Rais_model.php
@@ -18,4 +18,36 @@ class Rais_model extends CI_Model{
 		return $this->db->get('pasien');
 	}
 
+	public function input_data_obat($data)
+	{
+		return $this->db->insert('obat', $data);
+	}
+
+	public function ambil_data_obat()
+	{
+		$this->datatables->select('id_obat,nama_obat,harga_obat,stok_obat');
+		$this->datatables->from('obat');
+		$this->datatables->add_column('action', '<a href="javascript:void(0);" class="edit_record btn btn-info btn-xs" data-id="$1" data-nama="$2" data-harga="$3" data-stok="$4">Edit</a> <a href="javascript:void(0);" class="hapus_record bt btn-danger btn-sm" data-id="$1">Hapus</a>', 'id_obat,nama_obat,harga_obat,stok_obat');
+		
+		return $this->datatables->generate();
+	}
+
+	public function update_obat($data)
+	{
+		$this->db->set($data);
+		$this->db->where('id_obat', $data['id_obat']);
+		return $this->db->update('obat');
+	}
+
+	public function delete_obat($id)
+	{
+		$this->db->where('id_obat', $id);
+		$this->db->delete('obat');
+	}
+
+	public function input_data_tindakan($data)
+	{
+		return $this->db->insert('tindakan', $data);
+	}
+
 }
